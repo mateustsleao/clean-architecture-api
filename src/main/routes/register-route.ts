be8def7d6eecb4bf10866f2752d073dd549c02a4
mateustsleao <mateustsleao@gmail.com>
2022-01-25 20:02:20 -0300

    feat: add register and send email usecase

--- src/main/routes/register-route.ts
+++ src/main/routes/register-route.ts
@@ -3,5 +3,5 @@ import { makeRegisterUserController } from '@/main/factories/'
 import { adaptRoute } from '@/main/adapters/'
 
 export default (router: Router): void => {
-  router.post('/register/', adaptRoute(makeRegisterUserController()))
+  router.post('/register', adaptRoute(makeRegisterUserController()))
 }
